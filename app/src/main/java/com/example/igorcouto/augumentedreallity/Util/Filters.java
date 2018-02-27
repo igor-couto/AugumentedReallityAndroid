1c03d7c5ca71312797465609eb3e7e2210d2ae94
Igor Couto <igor.fcouto@gmail.com>
2018-02-26 23:29:09 -0300

    Remove unnecessary brackets

--- app/src/main/java/com/example/igorcouto/augumentedreallity/Util/Filters.java
+++ app/src/main/java/com/example/igorcouto/augumentedreallity/Util/Filters.java
@@ -7,13 +7,12 @@ public class Filters {
         float alpha = 0.03f;
 
         if (input == null || prev == null)
-            throw new NullPointerException("input and prev float arrays must be non-NULL");
+            throw new NullPointerException("Input and prev float arrays must be non-NULL");
         if (input.length != prev.length)
-            throw new IllegalArgumentException("input and prev must be the same length");
+            throw new IllegalArgumentException("Input and prev must be the same length");
 
-        for (int i = 0; i < input.length; i++) {
+        for (int i = 0; i < input.length; i++)
             prev[i] = prev[i] + alpha * (input[i] - prev[i]);
-        }
 
         return prev;
     }
